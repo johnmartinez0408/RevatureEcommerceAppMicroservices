9e189a725d9fc1358a72026c7d6a102d4cc6c984
John Martinez <johnmartinez0408@gmail.com>
2025-10-23 09:51:37 -0400

    added category service

---
+++ product-service/src/main/java/com/example/product_service/entity/Product.java
@@ -0,0 +1,26 @@
+package com.example.product_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(
+        name="products"
+)
+public class Product {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long productId;
+    private String productName;
+    private String productDescription;
+    private Long price;
+    private Long quantity;
+}
