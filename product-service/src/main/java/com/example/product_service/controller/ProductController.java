cccbd33b3bfb765a5b583905e58605f048c259a7
John Martinez <johnmartinez0408@gmail.com>
2025-10-23 13:34:20 -0400

    corrected missing dependencies, added missing controller for products, corrected error in CatController

--- product-service/src/main/java/com/example/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/example/product_service/controller/ProductController.java
@@ -1,4 +1,31 @@
 package com.example.product_service.controller;
 
+import com.example.product_service.entity.Product;
+import com.example.product_service.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Product getProductById(@PathVariable Long productId){
+        return productService.getProductById(productId);
+    }
+
+    @PostMapping
+    public Product saveProduct(@RequestBody Product product){
+        return productService.saveProduct(product);
+    }
 }
