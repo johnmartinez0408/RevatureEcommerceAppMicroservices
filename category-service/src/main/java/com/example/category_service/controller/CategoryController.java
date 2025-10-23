d1706b7363313910a1178403a775d7ee3a329a68
John Martinez <johnmartinez0408@gmail.com>
2025-10-23 10:13:44 -0400

    added controller endpoints

--- category-service/src/main/java/com/example/category_service/controller/CategoryController.java
+++ category-service/src/main/java/com/example/category_service/controller/CategoryController.java
@@ -1,10 +1,33 @@
 package com.example.category_service.controller;
 
+import com.example.category_service.entity.Category;
+import com.example.category_service.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/category")
 @Controller
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories(){
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Category getCategoryById(@PathVariable Long categoryId){
+        return categoryService.getCategoryById(categoryId);
+    }
+
+    @PostMapping
+    public Category saveCategory(@RequestBody Category category){
+        return categoryService.saveCategory(category);
+    }
 
 }
